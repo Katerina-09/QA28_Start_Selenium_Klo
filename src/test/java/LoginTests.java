11096eea0f62881fa022c9e8815a6872676bc48c
Katerina-09 <firiall68@gmail.com>
2025-01-26 19:29:53 +0200

    Added Tests

---
+++ src/test/java/LoginTests.java
@@ -0,0 +1,43 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+    @BeforeClass
+    public void preConditions(){
+        //open browser
+        //open site
+    }
+    @Test
+    public void loginSuccess(){
+        //open form (find element + click)
+        //fill email (find element + click + clear + tape)
+        //fill password (find element + click + clear + tape)
+        //submit form (find element + click)
+
+    }
+
+    @Test
+    public void loginWrongEmail(){
+        //open form (find element + click)
+        //fill email (find element + click + clear + tape)
+        //fill password (find element + click + clear + tape)
+        //submit form (find element + click)
+
+    }
+
+    @Test
+    public void loginWrongPassword(){
+        //open form (find element + click)
+        //fill email (find element + click + clear + tape)
+        //fill password (find element + click + clear + tape)
+        //submit form (find element + click)
+
+    }
+    @AfterClass
+    public void postConditions(){
+        //close browser
+    }
+
+}
